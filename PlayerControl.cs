385acb5a95463e32f04fd53a51fa43d7618b7d21
XanderAlmario <christian.almario@student.ateneo.edu>
2025-09-15 02:49:23 +0800

    Jump stuff

--- PlayerControl.cs
+++ PlayerControl.cs
@@ -5,37 +5,55 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     private float input, speed = 5f, jump = 12.5f;
-    private bool facingLeft;
+    private bool doubleJump = false;
+    private RaycastHit2D bottomCheck, hittingWall;
     public Rigidbody2D rb;
     public BoxCollider2D bc;
-    public LayerMask ground;
+    public LayerMask jumpable, wall;
+    public Animator animator;
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(groundCheck());
+        // Debug.Log(groundCheck());
         input = Input.GetAxis("Horizontal");
 
         rb.linearVelocity = new Vector2(input * speed, rb.linearVelocity.y);
 
-        if (input < -0.01f)
-        {
-            facingLeft = true;
-        }
-        else if (input > 0.01f)
-        {
-            facingLeft = false;
-        }
+        animator.SetFloat("Horizontal", input);
+        animator.SetFloat("Speed", Mathf.Abs(input));
+        // Speed is a general checker if the player is moving, regardless of direction
+        // Horizontal will determine where the player faces, -1 for left, 1 for right
 
-        if (Input.GetKey(KeyCode.Space) && groundCheck())
+        if (Mathf.Abs(animator.GetFloat("Horizontal")) > 0.00)
+            animator.SetFloat("LastHorizontal", input);
+        // LastHorizontal takes the direction the player faces when it moves and faces
+        // that same way when they stop moving and are idle
+        // it is defaulted at 1(right) because we want the player to start in idle right
+
+        bottomCheck = Physics2D.BoxCast(bc.bounds.center, bc.bounds.size, 0, Vector2.down, 0.01f, jumpable);
+        hittingWall = Physics2D.BoxCast(bc.bounds.center, bc.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.01f, wall);
+        if (Input.GetKeyDown(KeyCode.Space) && (bottomCheck || !doubleJump))
+        {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jump);
-    }
+            if (!bottomCheck)
+                doubleJump = true;
+        }
 
-    private bool groundCheck()
-    {
-        RaycastHit2D bottomCheck = Physics2D.BoxCast(bc.bounds.center, bc.bounds.size, 0, Vector2.down, 0.01f, ground);
+        if (hittingWall)
+        {
+            rb.gravityScale = 0.5f;
+            doubleJump = false;
+        }
+        else
+            rb.gravityScale = 3;  
+
         if (bottomCheck)
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
-        return bottomCheck;
+        {
+            doubleJump = false;
+            animator.SetBool("isJumping", false);
+        }
+        else
+            animator.SetBool("isJumping", true);
     }
 }
