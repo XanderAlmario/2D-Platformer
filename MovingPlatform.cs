76c719e353bd56a3b1051d904937c7155cacfd7d
Trisha Nicole Acero <161185081+tnacero@users.noreply.github.com>
2025-09-17 22:34:21 +0800

    Update MovingPlatform.cs

--- MovingPlatform.cs
+++ MovingPlatform.cs
@@ -24,4 +24,21 @@ public class MovingPlatform : MonoBehaviour
             nextPosition = (nextPosition == pointA.position) ? pointB.position : pointA.position;
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.parent = transform;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.parent = null;
+        }
+    }
 }
+
